68fd73c21bdfc964873eccf1fec564feb9acb310
abhiy562001 <161271427+abhiy562001@users.noreply.github.com>
2025-01-26 11:40:43 +0530

    code for login feature

---
+++ src/main/java/cucumber/VtigerAutomation/App.java
@@ -0,0 +1,13 @@
+package cucumber.VtigerAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/loginTest/ValidLoginTest.java
@@ -0,0 +1,25 @@
+package loginTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ValidLoginTest {
+
+	@Test
+	public void validLogin() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+
+		WebElement ele = driver.findElement(By.xpath("//input[@name='q']"));
+		ele.sendKeys("samsung Mobiles");
+		ele.sendKeys(Keys.ENTER);
+
+	}
+
+}
